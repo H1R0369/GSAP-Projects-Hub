3ffeb3e304b9fe15b674d8669e80e9cf9282b9df
Hiro <sonamtopgey369@gmail.com>
2025-09-18 20:06:09 +0530

    Add input slider

--- data/projects/plane-motion-path/index.js
+++ data/projects/plane-motion-path/index.js
@@ -4,10 +4,13 @@ const selectAll = e => document.querySelectorAll(e);
 const controlBtn = select('#control-btn');
 const revBtn = select('#reverse-btn');
 const resBtn = select('#restart-btn');
+const timeSlider = select('#time-slider');
 const progressEl = select('#progress');
 const timeEl = select('#time');
 
-const animation = gsap.to('img', {
+let flipped = false;
+
+const anim = gsap.to('img', {
     motionPath: {
         path: [
             {x: 250, y: 50},
@@ -18,38 +21,60 @@ const animation = gsap.to('img', {
         autoRotate: true
     },
     duration: 5,
-    repeat: -1,
-    yoyo: true,
-    ease: 'power1.inOut'
+    ease: 'power1.inOut',
 })
 
-let flipped = false;
-animation.eventCallback('onRepeat', function() {
+anim.eventCallback('onComplete', flipPlane, ['reverse']);
+
+anim.eventCallback('onReverseComplete', flipPlane, ['restart']);
+
+anim.eventCallback('onUpdate', function() {
+
+    timeSlider.value = this.time();
+    updateTimeValues(this.progress(), this.time());
+
+});
+
+function flipPlane(direction) {
 
     gsap.to(this.targets(), { scaleX: flipped ? 1 : -1, duration: 0.25 });
     
     if (flipped) {flipped = false}
     else flipped = true;
 
-});
+    if (direction === 'reverse') {this.reverse()}
+    else this.restart();
 
-animation.eventCallback('onUpdate', function() {
+};
 
-    progressEl.textContent = `Progress: ${this.progress().toFixed(1)}`;
-    timeEl.textContent = `Time: ${this.time().toFixed(1)}`
+function updateTimeValues(progressValue, timeValue) {
 
-})
+    progressEl.textContent = `Progress: ${progressValue.toFixed(1)}`;
+    timeEl.textContent = `Time: ${timeValue.toFixed(1)}`
+
+}
 
 controlBtn.addEventListener('click', (e) => {
-    animation.paused(!animation.paused());
-    e.target.textContent = animation.paused() ? 'play' : 'pause';
+    anim.paused(!anim.paused());
+    e.target.textContent = anim.paused() ? 'play' : 'pause';
 });
 
 revBtn.addEventListener('click', (e) => {
-    animation.reversed(!animation.reversed());
-    e.target.textContent = animation.reversed() ? 'forward' : 'reverse';
+    anim.reversed(!anim.reversed());
+    e.target.textContent = anim.reversed() ? 'forward' : 'reverse';
 });
 
 resBtn.addEventListener('click', () => {
-    animation.restart();
+    anim.restart();
+});
+
+timeSlider.addEventListener('input', (e) => {
+    const val = Number(e.target.value);
+    anim.pause(val);
+    updateTimeValues(anim.progress(), anim.time());
+})
+
+timeSlider.addEventListener('change', (e) => {
+    const val = Number(e.target.value);
+    anim.resume(val);
 })
