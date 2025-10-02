30c78243902b4782b87e494a8734f086cb335a0b
Hiro <sonamtopgey369@gmail.com>
2025-10-02 18:55:08 +0530

    Add projects

--- data/projects-list.js
+++ data/projects-list.js
@@ -4,36 +4,35 @@ class Project {
 
         this.title = projectDetails.title;
         this.description = projectDetails.description;
-        this.image = projectDetails.image;
+        this.imageSrc = projectDetails.imageSrc;
         this.href = projectDetails.href;
+    };
+};
 
-    }
-
-}
-
-export const projects = [
+const projects = [
 
     {
         title: 'Plane Motion Path',
         description: 'Interactive GSAP animation of a plane flying between home and an airport with motion path, auto-rotation, speed control, reversing, progress tracking, and point-to-click navigation',
-        image: 'plane-motion-path-thumbnail',
+        imageSrc: '../assets/images/thumbnails/plane-motion-path-thumbnail.png',
         href: './data/projects/plane-motion-path/index.html' 
     },
     {
         title: 'Hover Highlight Button',
         description: 'A button that hightlights when hovered over',
-        image: 'hover-highlight-button-thumbnail',
+        imageSrc: '../assets/images/thumbnails/hover-highlight-button-thumbnail.png',
         href: './data/projects/hover-highlight-button/index.html'
     },
     {
         title: 'Letter Hole Jumper',
         description: 'A simple animation of the letter H popping out of a hole that opens up and then closes.',
-        image: 'letter-hole-jumper-thumbnail',
+        imageSrc: '../assets/images/thumbnails/letter-hole-jumper-thumbnail.png',
         href: './data/projects/letter-hole-jumper/index.html'
-    }
+    },
 
 ].map(projectDetails => {
 
     return new Project(projectDetails)
+});
 
-})
+export default projects;
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,38 @@
 import gs from './gsap.js';
+import projects from '../data/projects-list.js';
+
+const projectsContainer = document.querySelector('#projects-container');
+
+function renderProjects() {
+
+    let projectsHTML = '';
+
+    projects.forEach(project => {
+
+        projectsHTML += `
+
+            <div class="project-card">
+                <a 
+                    href=${project.href}
+                    class="project-card-link"
+                >
+                    <img 
+                        src=${project.imageSrc}
+                        class="project-thumbnail"
+                        alt="project-thumbnail"
+                    >
+                    <h5>${project.title}</h5>
+                    <p>${project.description}</p>
+                </a>
+            </div>
+        `
+    });
+
+    projectsContainer.innerHTML = projectsHTML;
+}
 
 window.addEventListener('load', () => {
     gs.displayBody();
-    gs.playEntry()
+    gs.playEntry();
+    renderProjects();
 })
